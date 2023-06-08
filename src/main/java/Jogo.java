be08696eee340b153487fed0b0a51ead3f94f739
Pedro Debs <pedrodebs1@gmail.com>
2023-06-08 15:41:55 -0300

    melhora layouts, visual e lógica

--- src/main/java/Jogo.java
+++ src/main/java/Jogo.java
@@ -1,7 +1,8 @@
 import java.util.ArrayDeque;
 import java.sql.*;
 
-import javafx.application.Application;	
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
@@ -9,21 +10,56 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
+import javafx.event.*;
 
 public class Jogo extends Application {
+
+	private static class MsgEvent extends Event {
+		private String msg = null;
+		public static final EventType<MsgEvent> ALTERA_MSG = new EventType<>("ALTERA_MSG");
+
+		public MsgEvent () {
+			super(ALTERA_MSG);
+		}
+
+		public MsgEvent (String msg) {
+			super(ALTERA_MSG);
+			this.msg = msg;
+		}
+
+		public String getMsg() {
+			return this.msg;
+		}
+	}
+
+	private static final EventType<Event> TELA_INICIAL = new EventType<>("TELA_INICIAL");
+
 	private Personagem player1, player2;
+
 	
-	private StackPane fim() {
-			String s;
-			if (player1.getVida() == player2.getVida())
-				s = "EMPATE!";
-			else if (player1.getVida() > player2.getVida())
-				s = "VENCEDOR: " + player1.getNome();
-			else
-				s = "VENCEDOR: " + player2.getNome();
-			Text txt = new Text(s);
-			txt.setFont(new Font(50));
-			return new StackPane(txt);
+	private BorderPane fim() {
+		String s;
+		if (player1.getVida() == player2.getVida())
+			s = "EMPATE!";
+		else if (player1.getVida() > player2.getVida())
+			s = "VENCEDOR: " + player1.getNome();
+		else
+			s = "VENCEDOR: " + player2.getNome();
+		player1 = null;
+		player2 = null;
+
+		Text txt = new Text(s);
+		txt.setFont(new Font(50));
+
+		Button voltar = new Button("Voltar");
+		voltar.setOnAction(e -> voltar.fireEvent(new Event(TELA_INICIAL)));
+		BorderPane.setMargin(voltar, new Insets(15));
+
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(txt);
+		borderPane.setTop(voltar);
+
+		return borderPane;
 	}
 
 	private void escolher(Button b, Text txt) {
@@ -36,7 +72,7 @@ public class Jogo extends Application {
 		} else {
 			player2 = p; 
 			p.setStatus(false);
-			jogo(b.getScene());
+			b.getScene().setRoot(jogo());
 		}
 	}
 
@@ -48,8 +84,8 @@ public class Jogo extends Application {
 								  ProgressBar vida2)
 	{
 		if (perguntas.isEmpty()) {
-				botoes.getScene().setRoot(fim());
-				return;
+			botoes.getScene().setRoot(fim());
+			return;
 		}
 
 		Pergunta pergunta = perguntas.pop();
@@ -58,6 +94,7 @@ public class Jogo extends Application {
 
 		Button next = new Button("Próxima pergunta");
 		next.setOnAction(e -> {
+			next.fireEvent(new MsgEvent());
 			vbox.getChildren().remove(next);
 			displayPerguntas(perguntas, vbox, botoes, enunciado, vida1, vida2);
 		});
@@ -69,16 +106,26 @@ public class Jogo extends Application {
 		hbox.setAlignment(Pos.CENTER);
 		confirmacao.setAlignment(Pos.CENTER);
 		sim.setOnAction(e -> {
-			if (!player1.getResposta().getStatus()) {
+			boolean status1 = player1.getResposta().getStatus();
+			boolean status2 = player2.getResposta().getStatus();
+			if (!status1) {
 				player1.subVida(0.1);
 				if (player1.getVida() <= 0)
 					vbox.getScene().setRoot(fim());
 			}
-			if (!player2.getResposta().getStatus()) {
+			if (!status2) {
 				player2.subVida(0.1);
 				if (player1.getVida() <= 0)
 					vbox.getScene().setRoot(fim());
 			}
+
+			String msg;
+			if (!status1 && !status2)
+				msg = "Ambos os jogadores erraram!";
+			else
+				msg = "Jogador " + (status1 ? 1 : 2) + " acertou!";
+			sim.fireEvent(new MsgEvent(msg));
+
 			vida1.setProgress(player1.getVida());
 			vida2.setProgress(player2.getVida());
 			vbox.getChildren().remove(confirmacao);
@@ -97,55 +144,60 @@ public class Jogo extends Application {
 		});
 
 		nao.setOnAction(e -> {
-			vbox.getChildren().remove(confirmacao);
+			nao.fireEvent(new MsgEvent());
 			for (Node b : botoes.getChildren())
-					b.setStyle(null);
+				b.setStyle(null);
+			vbox.getChildren().remove(confirmacao);
 		});
 
-		botoes.getChildren().clear();
 		byte letra = 0;
+		botoes.getChildren().clear();
 		for (Alternativa a : pergunta.getAlternativas()) {
 			Button btn = new Button((char)('a' + letra) + ") " + a.getEnunciado());
+			btn.setMinWidth(500);
 			btn.setOnAction(e -> {
 				Personagem p = player1.getStatus() ? player1 : player2;
 				p.setResposta(a);
 				btn.setStyle("-fx-text-fill: #0000ff;");
 
-				if (p == player2) {
-					vbox.getChildren().add(confirmacao);
-				}
-
 				player1.toggleStatus();
 				player2.toggleStatus();
+
+				if (p == player2) {
+					vbox.getChildren().add(confirmacao);
+				} else {
+					btn.fireEvent(new MsgEvent());
+				}
 			});
 			letra = (byte)((letra + 1) % 26);
 			botoes.getChildren().add(btn);
 		}
 	}
 
-	private void jogo(Scene scene) {
+	private BorderPane jogo() {
 		ProgressBar vida1 = new ProgressBar();
 		ProgressBar vida2 = new ProgressBar();
+		HBox.setHgrow(vida1, Priority.ALWAYS);
+		HBox.setHgrow(vida2, Priority.ALWAYS);
 		vida1.setProgress(1);
 		vida2.setProgress(1);
 
-		HBox hbox1 = new HBox(
+		Text rodada = new Text("Vez do jogador 1");
+
+		HBox hbox = new HBox(
 			5,
 			new Text(player1.getNome()),
-			vida1
-		);
-		HBox hbox2 = new HBox(
-			5,
+			vida1,
+			rodada,
 			vida2,
 			new Text(player2.getNome())
 		);
-		hbox1.setAlignment(Pos.TOP_LEFT);
-		hbox2.setAlignment(Pos.TOP_RIGHT);
-		hbox1.setPadding(new Insets(15));
-		hbox2.setPadding(new Insets(15));
+		hbox.setAlignment(Pos.TOP_CENTER);
+		hbox.setPadding(new Insets(15));
 
-		vida1.prefWidthProperty().bind(hbox1.widthProperty().divide(2).subtract(100));
-		vida2.prefWidthProperty().bind(hbox2.widthProperty().divide(2).subtract(100));
+		ReadOnlyDoubleProperty tamanho = hbox.widthProperty();
+		vida1.prefWidthProperty().bind(tamanho);
+		vida2.prefWidthProperty().bind(tamanho);
 
 		Text enunciado = new Text();
 		enunciado.setFont(new Font(30));
@@ -179,13 +231,13 @@ public class Jogo extends Application {
 				try (
 					ResultSet ars = ps.executeQuery();
 				) {
-						while (ars.next()) {
-							Alternativa alternativa = new Alternativa(
-								ars.getString("enunciado"),
-								ars.getBoolean("status")
-							);
-							pergunta.addAlternativa(alternativa);
-						}
+					while (ars.next()) {
+						Alternativa alternativa = new Alternativa(
+							ars.getString("enunciado"),
+							ars.getBoolean("status")
+						);
+						pergunta.addAlternativa(alternativa);
+					}
 				}
 			}
 		} catch (SQLException e) {
@@ -194,15 +246,30 @@ public class Jogo extends Application {
 		}
 
 		displayPerguntas(perguntas, vbox, botoes, enunciado, vida1, vida2);
-		scene.setRoot(new StackPane(hbox1, hbox2, vbox));
+
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(hbox);
+		borderPane.setCenter(vbox);
+
+		borderPane.addEventHandler(MsgEvent.ALTERA_MSG, e -> {
+			String msg = e.getMsg();
+			if (msg == null) {
+				msg = "Vez do jogador " + (player1.getStatus() ? 1 : 2);
+			}
+			rodada.setText(msg);
+		});
+
+		return borderPane;
 	}
 
-	private StackPane telaDeSelecao() {
+	private BorderPane telaDeSelecao() {
 		Button personagem1 = new Button("Evandro");
 		Button personagem2 = new Button("Jorge");
 		Button personagem3 = new Button("Agnaldo");
+
 		Text titulo = new Text("Escolha o primeiro personagem");
 		titulo.setFont(new Font(25));
+
 		personagem1.setOnAction(e -> escolher(personagem1, titulo));
 		personagem2.setOnAction(e -> escolher(personagem2, titulo));
 		personagem3.setOnAction(e -> escolher(personagem3, titulo));
@@ -213,17 +280,25 @@ public class Jogo extends Application {
 			personagem2,
 			personagem3
 		);
+		hbox.setAlignment(Pos.CENTER);
 
 		VBox vbox = new VBox(
 			10,
 			titulo,
 			hbox
 		);
-
 		vbox.setAlignment(Pos.CENTER);
-		hbox.setAlignment(Pos.CENTER);
 
-		return new StackPane(vbox);
+		Button voltar = new Button("Voltar");
+		voltar.setCancelButton(true);
+		voltar.setOnAction(e -> voltar.fireEvent(new Event(TELA_INICIAL)));
+		BorderPane.setMargin(voltar, new Insets(15));
+
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(vbox);
+		borderPane.setTop(voltar);
+
+		return borderPane;
 	}
 
 	// Esse método tem que ser público
@@ -234,10 +309,6 @@ public class Jogo extends Application {
 		stage.setTitle("quizcc");
 		stage.setMaximized(true);
 
-		StackPane telaPrincipal = new StackPane();
-		Scene scene = new Scene(telaPrincipal);
-		scene.getStylesheets().add("style.css");
-			  
 		Text titulo = new Text("Quiz CC");
 		titulo.setFont(new Font(50));
 		titulo.setFill(Color.RED);
@@ -246,23 +317,32 @@ public class Jogo extends Application {
 		subtitulo.setFont(new Font(30));
 
 		Button novoJogo = new Button("Novo Jogo");
-		novoJogo.setOnAction(e ->
-			scene.setRoot(telaDeSelecao())
-		);
-		Button leaderboard = new Button("Leaderboard");
+		Button comoJogar = new Button("Como Jogar");
 		Button creditos = new Button("Créditos");
+		Button sair = new Button("Sair");
 
-		VBox vbox = new VBox(
+		VBox telaPrincipal = new VBox(
 			15,
 			titulo,
 			subtitulo,
 			novoJogo,
-			leaderboard,
-			creditos
+			comoJogar,
+			creditos,
+			sair
 		);
-		vbox.setAlignment(Pos.CENTER);
+		telaPrincipal.setAlignment(Pos.CENTER);
 
-		telaPrincipal.getChildren().add(vbox);
+		for (Node n : telaPrincipal.getChildren())
+			if (n instanceof Button) 
+				((Button)n).setMinWidth(150);
+
+		Scene scene = new Scene(telaPrincipal);
+		scene.getStylesheets().add("style.css");
+		scene.addEventHandler(TELA_INICIAL, e -> scene.setRoot(telaPrincipal));
+
+		novoJogo.setOnAction(e ->
+			scene.setRoot(telaDeSelecao())
+		);
 
 		stage.setScene(scene);
 		stage.show();
